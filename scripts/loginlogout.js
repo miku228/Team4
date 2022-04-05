12049c0d353ae5bdbaef1b6c1689e41fec0ec665
miku228 <avenir.lnlu@gmail.com>
2022-04-05 09:47:57 -0400

    login form by miku abe

---
+++ scripts/loginlogout.js
@@ -0,0 +1,18 @@
+var objUsers = [
+    {
+        username: "user01",
+        password: "algonquin01"
+    },
+    {
+        username: "user02",
+        password: "algonquin02"
+    },
+    {
+        username: "user03",
+        password: "algonquin03"
+    }
+];
+
+function login() {
+    $('input[type=text')
+}
