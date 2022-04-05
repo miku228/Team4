18ff9ff6d1ea8c63914d38654807649b6126f39b
jiyoungGithub <101651613+jiyoungGithub@users.noreply.github.com>
2022-04-05 03:33:15 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,14 @@
 and the left margin of the page content to 250px 
 and add a black background color to body */
 function openNav() {
-    document.querySelector('#mysidenav').style.width = "200px";
-    document.querySelector('main').style.width = "200px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.querySelector('#mySidenav').style.width = "200px";
+    document.querySelector('#main').style.marginLeft = "200px";  /* jiyoung modified April 4,2022*/
 }
 
 /* Set the width of the side navigation to 0 
 and the left margin of the page content to 0, 
 and the background color of body to white */
 function closeNav() {
-    document.querySelector('#mysidenav').style.width = "0";
-    document.querySelector('main').style.width = "0";
-    document.body.style.backgroundColor = "#FEFAE0";
+    document.querySelector('#mySidenav').style.width = "0";
+    document.querySelector('#main').style.marginLeft = "0";  /* jiyoung modified April 4,2022*/
 }
