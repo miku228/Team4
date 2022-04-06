0e96eeaeaf3a1ba2e0936d7bef1eca8a6c982bf6
miku228 <avenir.lnlu@gmail.com>
2022-04-06 11:08:03 -0400

    login-logout page by miku

--- scripts/loginlogout.js
+++ scripts/loginlogout.js
@@ -13,6 +13,50 @@ var objUsers = [
     }
 ];
 
-function login() {
-    $('input[type=text')
+function login(event) {
+     // cancel send form content to the URL
+    event.preventDefault();
+    let input = $('input');
+    let username = input[0].value;
+    let password = input[1].value;
+    input[0].nextElementSibling.textContent = '';
+    input[1].nextElementSibling.textContent = '';
+    
+    for(v of objUsers) {
+        if(username == v.username && password == v.password){
+            console.log('correct')
+            loginflag = true;
+            sessionStorage.setItem('userlogin', 'true');
+            // $('#logout').show()
+            // $('#loginform').hide()
+            location.reload()
+            return;
+        }
+    }
+    if(username == '') input[0].nextElementSibling.textContent = 'Username is empty';
+    if(password == '') {
+        input[1].nextElementSibling.textContent = 'Password is empty';
+    }else{
+        input[1].nextElementSibling.textContent = 'Username or password are wrong'
+
+    }
 }
+
+function logout() {
+    sessionStorage.removeItem('userlogin');
+    $('#loginform').show()
+    $('#logout').hide()
+}
+
+if(sessionStorage.getItem('userlogin')) {
+    $('#loginform').hide()
+    $('#logout').show()
+    
+}else{
+    $('#loginform').show()
+    $('#logout').hide()
+}
+
+$('#login').on('click', login);
+$('#logout').on('click', logout)
+
